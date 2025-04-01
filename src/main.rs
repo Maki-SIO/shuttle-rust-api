b9cac1d0d8008bd15d582f37c35928c207d3050c
Maki-SIO <maximiliengrz@gmail.com>
2025-04-01 10:00:51 +0200

    feat(login): Add login endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod model;
 
+use actix_web::web::Json;
 use actix_web::{get, post, web, HttpResponse};
+use jsonwebtoken::{encode, EncodingKey, Header};
 use model::User;
 use mongodb::{bson::doc, options::IndexOptions, Client, Collection, IndexModel};
+use serde::{Deserialize, Serialize};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::SecretStore;
 use shuttle_runtime::__internals::Context;
-use utoipa::OpenApi;
+use utoipa::{OpenApi, ToSchema};
 use utoipa_swagger_ui::SwaggerUi;
 
 const DB_NAME: &str = "preprod";
@@ -56,6 +59,73 @@ async fn get_user(client: web::Data<Client>, username: web::Path<String>) -> Htt
     }
 }
 
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize, ToSchema)]
+struct LoginRequest {
+    username: String,
+    password: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize, ToSchema)]
+struct Claims {
+    username: String,
+    exp: usize,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize, ToSchema)]
+struct LoginResponse {
+    token: String,
+}
+
+fn get_secret_key() -> String {
+    "4a77a9735cb3c1392399955f1c8d27b4f68c13832c574bf21ec7cfbc1c0fb663".to_string()
+}
+
+#[utoipa::path(
+    post,
+    path = "/login",
+    request_body = LoginRequest,
+    responses(
+        (status = 200, description = "Login successful", body = String),
+        (status = 401, description = "Unauthorized"),
+        (status = 500, description = "Internal Server Error")
+    )
+)]
+#[post("/login")]
+async fn login(client: web::Data<Client>, form: Json<LoginRequest>) -> HttpResponse {
+    let login_request = form.into_inner();
+    let collection = client.database(DB_NAME).collection::<User>(COLL_NAME);
+
+    match collection
+        .find_one(doc! { "username": &login_request.username })
+        .await
+    {
+        Ok(Some(user)) => match bcrypt::verify(&login_request.password, &user.pwd_hash) {
+            Ok(true) => {
+                let claims = Claims {
+                    username: user.username.clone(),
+                    exp: 10000000000,
+                };
+
+                let secret = get_secret_key();
+                match encode(
+                    &Header::default(),
+                    &claims,
+                    &EncodingKey::from_secret(secret.as_ref()),
+                ) {
+                    Ok(token) => HttpResponse::Ok()
+                        .content_type("application/json")
+                        .json(LoginResponse { token }),
+                    Err(_) => HttpResponse::InternalServerError().body("Error generating JWT"),
+                }
+            }
+            Ok(false) => HttpResponse::Unauthorized().body("Invalid password"),
+            Err(_) => HttpResponse::InternalServerError().body("Error verifying password"),
+        },
+        Ok(None) => HttpResponse::Unauthorized().body("User not found"),
+        Err(_) => HttpResponse::InternalServerError().body("Error finding user"),
+    }
+}
+
 async fn create_username_index(client: &Client) {
     let options = IndexOptions::builder().unique(true).build();
     let model = IndexModel::builder()
@@ -72,7 +142,7 @@ async fn create_username_index(client: &Client) {
 
 #[derive(OpenApi)]
 #[openapi(
-    paths(add_user, get_user),
+    paths(add_user, get_user, login),
     components(schemas(User)),
     tags(
         (name = "user", description = "User API")
@@ -97,9 +167,9 @@ async fn main(
         cfg.app_data(client_data.clone())
             .service(add_user)
             .service(get_user)
+            .service(login)
             .service(
-                SwaggerUi::new("/swagger-ui/{_:.*}")
-                    .url("/api-doc/openapi.json", ApiDoc::openapi()),
+                SwaggerUi::new("/docs/{_:.*}").url("/api-doc/openapi.json", ApiDoc::openapi()),
             );
     };
 
